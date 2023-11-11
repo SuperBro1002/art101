826f9ee2d3eada93e049dd6665a368b3c992ad4f
SuperBro1002 <pampudia@ucsc.edu>
2023-11-11 14:59:45 -0800

    Lab 9 bonus

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -3,26 +3,12 @@
  *  Created: 11/7/2023
  */
 
-// Adds buttons to each section of index.html
-$("#Challenges").append("<button id='pageButton'>CLICK");
-$("#Problems").append("<button id='pageButton2'>CLICK ME");
-$("#Results").append("<button id='pageButton3'>CLICK HERE!");
+// Adds a button to each section of index.html
+$(".minor-section").append("<button class='pageButton'>CLICK");
 
-//Adds eventlisteners to buttons
-$("#pageButton").click(
+//Adds eventlisteners to all buttons
+$(".pageButton").click(
     function () {
-        $("#Challenges").css("background-color", "green");
-    }
-);
-
-$("#pageButton2").click(
-    function () {
-        $("#Problems").css("background-color", "blue");
-    }
-);
-
-$("#pageButton3").click(
-    function () {
-        $("#Results").css("background-color", "red");
+        $(this).parent().toggleClass("special");
     }
 );
