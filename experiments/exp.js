d8174f7bf2ace7118e3e173d9843b4222d978521
SuperBro1002 <pampudia@ucsc.edu>
2023-11-16 08:36:35 -0800

    Lab10 bonus, exp

---
+++ experiments/exp.js
@@ -0,0 +1,4 @@
+$("#my-button").click(function(){
+    var name = prompt("Give your name now: ");
+    $("#title").html("Hello " + name);
+})
--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -6,7 +6,7 @@
 function makeRandomText() {
     const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
     const min = 3;
-    const max = 100;
+    const max = 150;
     const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
     // Get a random starting index to slice the Lorem Ipsum text
     const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
